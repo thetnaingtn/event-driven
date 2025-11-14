8eb7ab58efaf1f566af2dd2d44f3b48e1fe0559f
thetnaintn <thetnaingtun.ucsy@gmail.com>
2025-11-14 16:48:19 +0700

    04-router: 04-project-graceful-shutdown

--- project/service/service.go
+++ project/service/service.go
@@ -4,9 +4,11 @@ import (
 	"context"
 	"errors"
 	stdHTTP "net/http"
+	"os/signal"
 
 	"github.com/ThreeDotsLabs/watermill"
 	wMessage "github.com/ThreeDotsLabs/watermill/message"
+	"golang.org/x/sync/errgroup"
 
 	"github.com/labstack/echo/v4"
 	"github.com/redis/go-redis/v9"
@@ -43,15 +45,30 @@ func New(
 }
 
 func (s Service) Run(ctx context.Context) error {
-	go func() {
-		if err := s.router.Run(context.Background()); err != nil {
-			panic(err)
-		}
-	}()
-	err := s.echoRouter.Start(":8080")
-	if err != nil && !errors.Is(err, stdHTTP.ErrServerClosed) {
-		return err
-	}
+	ctx, cancel := signal.NotifyContext(ctx)
+	defer cancel()
 
-	return nil
+	g, ctx := errgroup.WithContext(ctx)
+
+	g.Go(func() error {
+		if err := s.router.Run(ctx); err != nil {
+			return err
+		}
+		return nil
+	})
+
+	g.Go(func() error {
+		err := s.echoRouter.Start(":8080")
+		if err != nil && !errors.Is(err, stdHTTP.ErrServerClosed) {
+			return err
+		}
+		return err
+	})
+
+	g.Go(func() error {
+		<-ctx.Done()
+		return s.echoRouter.Shutdown(ctx)
+	})
+
+	return g.Wait()
 }
